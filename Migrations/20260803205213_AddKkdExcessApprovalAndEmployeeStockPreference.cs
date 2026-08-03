1572d28bbf94254884d089b7e0308b79cd67d879
Can Nasıf <can.nasif@v3rii.com>
2026-08-04 01:24:31 +0300

    feat: migration - KKD fazla kota onay ve çalışan stok tercihi tablosu

---
+++ Migrations/20260803205213_AddKkdExcessApprovalAndEmployeeStockPreference.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace verii_wms_api_v2.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddKkdExcessApprovalAndEmployeeStockPreference : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ProjectCode",
+                table: "RII_WT_HEADER",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "RequireManagerApprovalForExcess",
+                table: "RII_KKD_POLICY",
+                type: "bit",
+                nullable: false,
+                defaultValue: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ExcessApprovalReason",
+                table: "RII_KKD_DISTRIBUTION",
+                type: "nvarchar(1000)",
+                maxLength: 1000,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ExcessApprovalStatus",
+                table: "RII_KKD_DISTRIBUTION",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: false,
+                defaultValue: "NotRequired");
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "ExcessApprovedAtUtc",
+                table: "RII_KKD_DISTRIBUTION",
+                type: "datetimeoffset",
+                nullable: true);
+
+            migrationBuilder.AddColumn<long>(
+                name: "ExcessApprovedBy",
+                table: "RII_KKD_DISTRIBUTION",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "RII_KKD_EMPLOYEE_STOCK_PREFERENCE",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EmployeeId = table.Column<long>(type: "bigint", nullable: false),
+                    GroupCode = table.Column<string>(type: "nvarchar(80)", maxLength: 80, nullable: false),
+                    StockId = table.Column<long>(type: "bigint", nullable: false),
+                    LastSelectedAtUtc = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    RowVersion = table.Column<byte[]>(type: "rowversion", rowVersion: true, nullable: false),
+                    BranchCode = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false, defaultValue: "0"),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    UpdatedDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DeletedDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false, defaultValue: false),
+                    CreatedBy = table.Column<long>(type: "bigint", nullable: true),
+                    UpdatedBy = table.Column<long>(type: "bigint", nullable: true),
+                    DeletedBy = table.Column<long>(type: "bigint", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RII_KKD_EMPLOYEE_STOCK_PREFERENCE", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RII_KKD_EMPLOYEE_STOCK_PREFERENCE_RII_KKD_EMPLOYEE_EmployeeId",
+                        column: x => x.EmployeeId,
+                        principalTable: "RII_KKD_EMPLOYEE",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RII_KKD_EMPLOYEE_STOCK_PREFERENCE_BranchCode_EmployeeId_GroupCode",
+                table: "RII_KKD_EMPLOYEE_STOCK_PREFERENCE",
+                columns: new[] { "BranchCode", "EmployeeId", "GroupCode" },
+                unique: true,
+                filter: "[IsDeleted] = 0");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RII_KKD_EMPLOYEE_STOCK_PREFERENCE_BranchCode_StockId",
+                table: "RII_KKD_EMPLOYEE_STOCK_PREFERENCE",
+                columns: new[] { "BranchCode", "StockId" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RII_KKD_EMPLOYEE_STOCK_PREFERENCE_EmployeeId",
+                table: "RII_KKD_EMPLOYEE_STOCK_PREFERENCE",
+                column: "EmployeeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RII_KKD_EMPLOYEE_STOCK_PREFERENCE_IsDeleted",
+                table: "RII_KKD_EMPLOYEE_STOCK_PREFERENCE",
+                column: "IsDeleted");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RII_KKD_EMPLOYEE_STOCK_PREFERENCE");
+
+            migrationBuilder.DropColumn(
+                name: "ProjectCode",
+                table: "RII_WT_HEADER");
+
+            migrationBuilder.DropColumn(
+                name: "RequireManagerApprovalForExcess",
+                table: "RII_KKD_POLICY");
+
+            migrationBuilder.DropColumn(
+                name: "ExcessApprovalReason",
+                table: "RII_KKD_DISTRIBUTION");
+
+            migrationBuilder.DropColumn(
+                name: "ExcessApprovalStatus",
+                table: "RII_KKD_DISTRIBUTION");
+
+            migrationBuilder.DropColumn(
+                name: "ExcessApprovedAtUtc",
+                table: "RII_KKD_DISTRIBUTION");
+
+            migrationBuilder.DropColumn(
+                name: "ExcessApprovedBy",
+                table: "RII_KKD_DISTRIBUTION");
+        }
+    }
+}
---
+++ Migrations/20260803205213_AddKkdExcessApprovalAndEmployeeStockPreference.sql
@@ -0,0 +1,132 @@
+﻿BEGIN TRANSACTION;
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260803205213_AddKkdExcessApprovalAndEmployeeStockPreference'
+)
+BEGIN
+    IF COL_LENGTH(N'dbo.RII_WT_HEADER', N'ProjectCode') IS NULL
+        ALTER TABLE [RII_WT_HEADER] ADD [ProjectCode] nvarchar(50) NULL;
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260803205213_AddKkdExcessApprovalAndEmployeeStockPreference'
+)
+BEGIN
+    IF COL_LENGTH(N'dbo.RII_KKD_POLICY', N'RequireManagerApprovalForExcess') IS NULL
+        ALTER TABLE [RII_KKD_POLICY] ADD [RequireManagerApprovalForExcess] bit NOT NULL DEFAULT CAST(1 AS bit);
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260803205213_AddKkdExcessApprovalAndEmployeeStockPreference'
+)
+BEGIN
+    IF COL_LENGTH(N'dbo.RII_KKD_DISTRIBUTION', N'ExcessApprovalReason') IS NULL
+        ALTER TABLE [RII_KKD_DISTRIBUTION] ADD [ExcessApprovalReason] nvarchar(1000) NULL;
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260803205213_AddKkdExcessApprovalAndEmployeeStockPreference'
+)
+BEGIN
+    IF COL_LENGTH(N'dbo.RII_KKD_DISTRIBUTION', N'ExcessApprovalStatus') IS NULL
+        ALTER TABLE [RII_KKD_DISTRIBUTION] ADD [ExcessApprovalStatus] nvarchar(30) NOT NULL DEFAULT N'NotRequired';
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260803205213_AddKkdExcessApprovalAndEmployeeStockPreference'
+)
+BEGIN
+    IF COL_LENGTH(N'dbo.RII_KKD_DISTRIBUTION', N'ExcessApprovedAtUtc') IS NULL
+        ALTER TABLE [RII_KKD_DISTRIBUTION] ADD [ExcessApprovedAtUtc] datetimeoffset NULL;
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260803205213_AddKkdExcessApprovalAndEmployeeStockPreference'
+)
+BEGIN
+    IF COL_LENGTH(N'dbo.RII_KKD_DISTRIBUTION', N'ExcessApprovedBy') IS NULL
+        ALTER TABLE [RII_KKD_DISTRIBUTION] ADD [ExcessApprovedBy] bigint NULL;
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260803205213_AddKkdExcessApprovalAndEmployeeStockPreference'
+)
+BEGIN
+    IF OBJECT_ID(N'dbo.RII_KKD_EMPLOYEE_STOCK_PREFERENCE', N'U') IS NULL
+    CREATE TABLE [RII_KKD_EMPLOYEE_STOCK_PREFERENCE] (
+        [Id] bigint NOT NULL IDENTITY,
+        [EmployeeId] bigint NOT NULL,
+        [GroupCode] nvarchar(80) NOT NULL,
+        [StockId] bigint NOT NULL,
+        [LastSelectedAtUtc] datetimeoffset NOT NULL,
+        [RowVersion] rowversion NOT NULL,
+        [BranchCode] nvarchar(10) NOT NULL DEFAULT N'0',
+        [CreatedDate] datetime2 NULL,
+        [UpdatedDate] datetime2 NULL,
+        [DeletedDate] datetime2 NULL,
+        [IsDeleted] bit NOT NULL DEFAULT CAST(0 AS bit),
+        [CreatedBy] bigint NULL,
+        [UpdatedBy] bigint NULL,
+        [DeletedBy] bigint NULL,
+        CONSTRAINT [PK_RII_KKD_EMPLOYEE_STOCK_PREFERENCE] PRIMARY KEY ([Id]),
+        CONSTRAINT [FK_RII_KKD_EMPLOYEE_STOCK_PREFERENCE_RII_KKD_EMPLOYEE_EmployeeId] FOREIGN KEY ([EmployeeId]) REFERENCES [RII_KKD_EMPLOYEE] ([Id]) ON DELETE NO ACTION
+    );
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260803205213_AddKkdExcessApprovalAndEmployeeStockPreference'
+)
+BEGIN
+    IF OBJECT_ID(N'dbo.RII_KKD_EMPLOYEE_STOCK_PREFERENCE', N'U') IS NOT NULL
+       AND NOT EXISTS (SELECT 1 FROM sys.indexes WHERE [object_id] = OBJECT_ID(N'dbo.RII_KKD_EMPLOYEE_STOCK_PREFERENCE') AND [name] = N'IX_RII_KKD_EMPLOYEE_STOCK_PREFERENCE_BranchCode_EmployeeId_GroupCode')
+        EXEC(N'CREATE UNIQUE INDEX [IX_RII_KKD_EMPLOYEE_STOCK_PREFERENCE_BranchCode_EmployeeId_GroupCode] ON [RII_KKD_EMPLOYEE_STOCK_PREFERENCE] ([BranchCode], [EmployeeId], [GroupCode]) WHERE [IsDeleted] = 0');
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260803205213_AddKkdExcessApprovalAndEmployeeStockPreference'
+)
+BEGIN
+    IF OBJECT_ID(N'dbo.RII_KKD_EMPLOYEE_STOCK_PREFERENCE', N'U') IS NOT NULL
+       AND NOT EXISTS (SELECT 1 FROM sys.indexes WHERE [object_id] = OBJECT_ID(N'dbo.RII_KKD_EMPLOYEE_STOCK_PREFERENCE') AND [name] = N'IX_RII_KKD_EMPLOYEE_STOCK_PREFERENCE_BranchCode_StockId')
+        EXEC(N'CREATE INDEX [IX_RII_KKD_EMPLOYEE_STOCK_PREFERENCE_BranchCode_StockId] ON [RII_KKD_EMPLOYEE_STOCK_PREFERENCE] ([BranchCode], [StockId])');
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260803205213_AddKkdExcessApprovalAndEmployeeStockPreference'
+)
+BEGIN
+    IF OBJECT_ID(N'dbo.RII_KKD_EMPLOYEE_STOCK_PREFERENCE', N'U') IS NOT NULL
+       AND NOT EXISTS (SELECT 1 FROM sys.indexes WHERE [object_id] = OBJECT_ID(N'dbo.RII_KKD_EMPLOYEE_STOCK_PREFERENCE') AND [name] = N'IX_RII_KKD_EMPLOYEE_STOCK_PREFERENCE_EmployeeId')
+        EXEC(N'CREATE INDEX [IX_RII_KKD_EMPLOYEE_STOCK_PREFERENCE_EmployeeId] ON [RII_KKD_EMPLOYEE_STOCK_PREFERENCE] ([EmployeeId])');
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260803205213_AddKkdExcessApprovalAndEmployeeStockPreference'
+)
+BEGIN
+    IF OBJECT_ID(N'dbo.RII_KKD_EMPLOYEE_STOCK_PREFERENCE', N'U') IS NOT NULL
+       AND NOT EXISTS (SELECT 1 FROM sys.indexes WHERE [object_id] = OBJECT_ID(N'dbo.RII_KKD_EMPLOYEE_STOCK_PREFERENCE') AND [name] = N'IX_RII_KKD_EMPLOYEE_STOCK_PREFERENCE_IsDeleted')
+        EXEC(N'CREATE INDEX [IX_RII_KKD_EMPLOYEE_STOCK_PREFERENCE_IsDeleted] ON [RII_KKD_EMPLOYEE_STOCK_PREFERENCE] ([IsDeleted])');
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260803205213_AddKkdExcessApprovalAndEmployeeStockPreference'
+)
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20260803205213_AddKkdExcessApprovalAndEmployeeStockPreference', N'10.0.10');
+END;
+
+COMMIT;
+GO
