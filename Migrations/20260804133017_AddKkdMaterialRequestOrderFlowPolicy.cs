3e3aea8b2d0a864b31211fd2d7d4a45fa63e18a3
Can Nasıf <can.nasif@v3rii.com>
2026-08-04 18:08:00 +0300

    feat: migration - KKD malzeme talep sipariş akışı politika alanı

---
+++ Migrations/20260804133017_AddKkdMaterialRequestOrderFlowPolicy.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace verii_wms_api_v2.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddKkdMaterialRequestOrderFlowPolicy : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "EnableMaterialRequestOrderFlow",
+                table: "RII_KKD_POLICY",
+                type: "bit",
+                nullable: false,
+                defaultValue: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EnableMaterialRequestOrderFlow",
+                table: "RII_KKD_POLICY");
+        }
+    }
+}
